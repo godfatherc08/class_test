037fa681b99549c19c2a6ff10b9a80d2bfec2437
godfatherc08 <139884797+godfatherc08@users.noreply.github.com>
2024-08-23 19:38:12 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+/*1. An array is an object which acts as a data structure, storing all kinds of data types irrespective of the type.
+It has multiple methods and uses zero-indexing. It is also mutable
+2. a.  .push
+b.  .include
+c.   .sort
+d.   .pop
+e.   .length
+*/
+
+
+const dummy_data = ["John", "Kehlani", "Barba", "Joe", "Chimaobi"]
+
+console.log(dummy_data.sort())
+console.log(dummy_data.pop())
